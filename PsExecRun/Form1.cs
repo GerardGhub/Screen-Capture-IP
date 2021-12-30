ae1404396f2e61e63782df210b348e4a51be9d92
GerardGHub <singian28@gmail.com>
2021-12-30 21:27:40 +0800

    first commit

---
+++ PsExecRun/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace PsExecRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string remoteMachine = textBox1.Text.Trim();
+            string batpath = textBox2.Text.Trim();
+            //string qwewqeqw = "netsh advfirewall firewall set rule name=" +"Remote Service Management(RPC)"+ " profile=domain new enable=yes";
+            string appName = "PsExec.exe";
+            string args = string.Format("\\\\{0} -u philip.calugcugan -p jp120193 -d -s -i cmd /c " + batpath + "", remoteMachine);
+            // Process.Start(appName, args);
+
+            MyProcess proc = new MyProcess();
+
+            ProcessStartInfo cmd = new ProcessStartInfo();
+
+            cmd.FileName = appName;
+            cmd.WindowStyle = ProcessWindowStyle.Normal;
+            cmd.Arguments = args;
+
+            proc.StartInfo = cmd;
+            proc.Exited += new EventHandler(myProcess_HasExited);
+            proc.Start();
+
+            //while (string.IsNullOrEmpty(proc.MainWindowTitle))
+            //{
+            //    MessageBox.Show("Wait");
+            //    System.Threading.Thread.Sleep(100);
+
+                
+            //}
+
+            proc.WaitForExit();
+
+
+            //proc.Stop();
+           // proc = Process.Start(cmd);
+
+           // proc.WaitForInputIdle();
+          //  proc.WaitForExit();
+        }
+
+        class MyProcess : Process
+        {
+            public void Stop()
+            {
+                this.CloseMainWindow();
+                this.Close();
+                OnExited();
+            }
+        }
+
+        private static void myProcess_HasExited(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Done!");
+        }
+
+    }
+}
---
+++ ScreenCap/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using xClasses;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ScreenCap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            etc123 qwe = new etc123();
+
+
+            bool sw = false;
+            string _myIp = qwe.GetLocalIPv4();
+            string path = DateTime.Now.ToString("yyyyMMdd") + "(" + _myIp + ")" + ".png";
+
+            string ss = Regex.Replace(_myIp, @"\.+", "_");
+
+            Thread.Sleep(300);
+
+            try
+            {
+                Rectangle bounds = Screen.GetBounds(Point.Empty);
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                    }
+                    bitmap.Save(@"C:\\sc\\" + path, ImageFormat.Png);
+                }
+                sw = true;
+            }
+            catch (Exception ex)
+            {
+                insert_logs("failed screenshot " + path);
+            }
+           
+            if (sw)
+            {
+                insert_logs("success screenshot " + path);
+            }
+        }
+
+        static void insert_logs(string logs)
+        {
+
+            try
+            {
+                const string location = @"scl";
+
+                if (!File.Exists(location))
+                {
+                    var createText = "New Activities Logs" + Environment.NewLine;
+                    File.WriteAllText(location, createText);
+                }
+                var appendLogs = "Activities Logs: " + logs + " " + DateTime.Now + Environment.NewLine;
+                File.AppendAllText(location, appendLogs);
+            }
+            catch (Exception ex)
+            {
+                const string location = @"scl";
+                if (!File.Exists(location))
+                {
+                    TextWriter file = File.CreateText(@"C:\ponLogs");
+                    var createText = "New Activities Logs" + Environment.NewLine;
+
+                    File.WriteAllText(location, createText);
+
+                }
+                var appendLogs = ex.Message + logs + DateTime.Now + Environment.NewLine;
+                File.AppendAllText(location, appendLogs);
+
+            }
+        }
+    }
+}
